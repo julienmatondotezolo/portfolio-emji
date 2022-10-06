0d951a3b73932c91c322bd5c7106220a1a53ba42
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-10-06 15:26:11 +0200

    adding circle animation & pictue

--- src/components/backgroundCircles/index.tsx
+++ src/components/backgroundCircles/index.tsx
@@ -3,12 +3,25 @@ import React from "react";
 
 export function BackgroundCircles() {
   return (
-    <div className="relative flex justify-center items-center">
-      <div className="absolute border border-[#404040] rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        scale: [1, 3, 1],
+        opacity: [0.2, 0.4, 1],
+        borderRadius: ["20%", "150%", "20%"],
+      }}
+      transition={{
+        duration: 2,
+      }}
+      className="relative flex justify-center items-center"
+    >
+      <div className="absolute border border-[#303030] rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
       <div className="absolute border border-[#303030] rounded-full h-[300px] w-[300px] mt-52" />
-      <div className="absolute border border-[#303030] rounded-full h-[500px] w-[500px] mt-52" />
+      <div className="absolute border border-[#272727] rounded-full h-[500px] w-[500px] mt-52" />
       <div className="absolute border border-[#cf081b] opacity-20 rounded-full h-[650px] w-[650px] mt-52 animate-pulse" />
       <div className="absolute border border-[#252525] rounded-full h-[800px] w-[800px] mt-52" />
-    </div>
+    </motion.div>
   );
 }
--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 
+import mypic from "../../assets/images/IMG_8258.png";
 import { BackgroundCircles } from "../";
 
 export function Hero() {
@@ -13,10 +16,35 @@ export function Hero() {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
-      <h1>
-        <span>{`< ${text} />`}</span>
-        <Cursor cursorColor="red" />
-      </h1>
+      <Image
+        className="relative rounded-full mx-auto"
+        src={mypic}
+        alt="Picture of the author"
+        width="220px"
+        height="220px"
+        objectFit="cover"
+      />
+      <article>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Full stack developer</h2>
+        <h1>
+          <span className="text-5xl lg:text-6xl font-semibold scroll-px-10">{`< ${text} />`}</span>
+          <Cursor cursorColor="red" />
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </article>
     </div>
   );
 }
