570904d7d6ed827b0156a3c1252df0ff0268837d
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-10-06 15:55:41 +0200

    adding custom css components & finish hero animation

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -16,35 +17,67 @@ export function Hero() {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
-      <Image
-        className="relative rounded-full mx-auto"
-        src={mypic}
-        alt="Picture of the author"
-        width="220px"
-        height="220px"
-        objectFit="cover"
-      />
-      <article>
-        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Full stack developer</h2>
+      <motion.div
+        initial={{
+          y: 200,
+          opacity: 0,
+          scale: 0,
+        }}
+        animate={{
+          y: [200, -80, -40],
+          x: 0,
+          opacity: 1,
+          scale: [0, 1.5, 0.8],
+        }}
+        transition={{
+          duration: 2,
+        }}
+        className="mx-auto"
+      >
+        <Image
+          className="rounded-full"
+          src={mypic}
+          alt="Picture of the author"
+          width="220px"
+          height="220px"
+          objectFit="cover"
+        />
+      </motion.div>
+      <motion.article
+        initial={{
+          opacity: 0,
+          scale: 0.8,
+        }}
+        animate={{
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          delay: 1.5,
+          duration: 1,
+        }}
+        className="z-20"
+      >
+        <h2 className="md:text-sm uppercase text-gray-500 pb-2 tracking-[15px] sm:text-xs">Full stack developer</h2>
         <h1>
           <span className="text-5xl lg:text-6xl font-semibold scroll-px-10">{`< ${text} />`}</span>
           <Cursor cursorColor="red" />
         </h1>
         <div className="pt-5">
           <Link href="#about">
-            <button className="heroButton">About</button>
+            <button className="heroButton mx-5 my-1">About</button>
           </Link>
           <Link href="#experience">
-            <button className="heroButton">Experience</button>
+            <button className="heroButton mx-5 my-1">Experience</button>
           </Link>
           <Link href="#skills">
-            <button className="heroButton">Skills</button>
+            <button className="heroButton mx-5 my-1">Skills</button>
           </Link>
           <Link href="#projects">
-            <button className="heroButton">Projects</button>
+            <button className="heroButton mx-5 my-1">Projects</button>
           </Link>
         </div>
-      </article>
+      </motion.article>
     </div>
   );
 }
