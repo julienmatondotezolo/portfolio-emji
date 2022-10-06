ee49392b9963ac2564f0e590de76654c35afe27b
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-10-06 13:35:06 +0200

    adding social icons

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { SocialIcon } from "react-social-icons";
 
 type Props = {};
 
 export function Header({}: Props) {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div>
+        <SocialIcon url="https://twitter.com/jaketrent" />
+      </div>
+    </header>
+  );
 }
