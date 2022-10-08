020646529db63aea89fbf931984a4c5b905caefd
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-10-08 21:54:19 +0200

    adding button variable

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -3,30 +3,44 @@ import React from "react";
 
 export function Contact() {
   return (
-    <div className="h-screen relative flex flex-col text-center md:text-left max-w-7xl justify-evenly mx-auto items-center z-0 px-5 md:px-10">
-      <h3 className="absolute top-24 brand-title">Contact</h3>
+    <div className="h-screen relative flex flex-col text-center max-w-7xl justify-evenly mx-auto items-center z-0 px-5 md:px-10 box-border">
+      <h3 className="absolute top-12 brand-title">Contact</h3>
 
-      <div className="space-y-10">
-        <h4 className="text-2xl font-semibold">
+      <div className="space-y-10 w-full">
+        <h4 className="text-2xl text-left md:text-center font-semibold">
           I got just what you need. Let&apos;s work <span className="text-primary-color">together.</span>
         </h4>
 
         <div className="space-y-5">
-          <div className="flex items-center space-x-5">
-            <PhoneIcon className="text-primary-color h-7 w-7 animate-bounce" />
+          <div className="flex items-center space-x-5 md:justify-center">
+            <PhoneIcon className="text-primary-color h-7 w-7 animate-pulse" />
             <p className="text-xl">+32 484 58 79 11</p>
           </div>
 
-          <div className="flex items-center space-x-5">
-            <EnvelopeIcon className="text-primary-color h-7 w-7 animate-bounce" />
+          <div className="flex items-center space-x-5 md:justify-center">
+            <EnvelopeIcon className="text-primary-color h-7 w-7 animate-pulse" />
             <p className="text-xl">info@emji.com</p>
           </div>
 
-          <div className="flex items-center space-x-5">
-            <MapPinIcon className="text-primary-color h-7 w-7 animate-bounce" />
+          <div className="flex items-center space-x-5 md:justify-center">
+            <MapPinIcon className="text-primary-color h-7 w-7 animate-pulse" />
             <p className="text-xl">Dilbeek</p>
           </div>
         </div>
+
+        <form className="flex flex-col space-y-2 w-fit mx-auto">
+          <div className="flex space-x-2">
+            <input placeholder="Name" className="contactInput" type="text" />
+            <input placeholder="Email" className="contactInput" type="text" />
+          </div>
+
+          <input placeholder="Subject" className="contactInput" type="text" />
+
+          <textarea placeholder="Message" className="contactInput" />
+          <button className="primaryButton" type="submit">
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
