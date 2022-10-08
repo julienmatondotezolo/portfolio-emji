b6dea6c9fc7615e4017af59974cb71b5aeccb97f
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-10-09 01:38:48 +0200

    background position change

--- src/components/backgroundCircles/index.tsx
+++ src/components/backgroundCircles/index.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export function BackgroundCircles() {
   return (
@@ -18,7 +18,7 @@ export function BackgroundCircles() {
       className="relative flex justify-center items-center"
     >
       <div className="absolute border border-[#303030] rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
-      <div className="absolute border border-[#303030] rounded-full h-[300px] w-[300px] mt-52" />
+      <div className="absolute translate-x-0 translate-y-0 border border-[#303030] rounded-full h-[300px] w-[300px] mt-52" />
       <div className="absolute border border-[#272727] rounded-full h-[500px] w-[500px] mt-52" />
       <div className="absolute border border-[#cf081b] opacity-20 rounded-full h-[650px] w-[650px] mt-52 animate-pulse" />
       <div className="absolute border border-[#252525] rounded-full h-[800px] w-[800px] mt-52" />
