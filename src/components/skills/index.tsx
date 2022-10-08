38a3974b999c99eb18acd475b003239e82b068e3
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-10-09 00:52:15 +0200

    adding mediaqueries to grid

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -16,7 +16,7 @@ export function Skills() {
 
       <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500">Hover over on of my skills</h3>
 
-      <div className="absolute top-52 grid grid-cols-4 gap-5">
+      <div className="absolute top-52 grid grid-cols-4 md:grid-cols-6 gap-5">
         <SkilssCard />
         <SkilssCard />
         <SkilssCard />
