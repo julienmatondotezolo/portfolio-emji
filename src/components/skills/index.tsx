ba793a68fc4dd53f110875891dc791ea727b8f14
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-10-09 01:19:49 +0200

    fixing skills responsive

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -10,11 +10,11 @@ export function Skills() {
       whileInView={{ opacity: 1 }}
       viewport={{ once: false }}
       transition={{ duration: 1.5 }}
-      className="flex relative flex-col text-center px-10 md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
+      className="flex relative flex-col text-center px-5 md:px-10 md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
     >
       <h3 className="absolute top-24 brand-title">Skills</h3>
 
-      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500">Hover over on of my skills</h3>
+      <h3 className="absolute top-36 uppercase tracking-[3px] text-primary-color-500">Hover over on of my skills</h3>
 
       <div className="absolute top-52 grid grid-cols-4 md:grid-cols-6 gap-5">
         <SkilssCard />
--- src/components/skilssCard/index.tsx
+++ src/components/skilssCard/index.tsx
@@ -6,16 +6,18 @@ import mypic from "../../assets/images/IMG_8258.png";
 
 export function SkilssCard() {
   return (
-    <figure className="group relative flex flex-col items-center object-cover cursor-pointer">
-      <div className="rounded-full bg-[#252525] h-16 w-16 md:w-28 xl:w-32 xl:h-32">
+    <figure className="group relative flex flex-col space-y-2 items-center object-cover cursor-pointer">
+      <div className="rounded-full bg-[#252525] h-12 w-12 md:w-28 xl:w-32 xl:h-32">
         <Image className="rounded-full" src={mypic} alt="Picture of the author" objectFit="cover" />
       </div>
 
-      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-full w-full rounded-full z-0">
+      <div className="absolute top-[-10px] opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-12 w-12 md:w-28 xl:w-32 xl:h-32 rounded-full z-0">
         <div className="flex items-center justify-center h-full">
-          <p className="text-xl font-bold text-primary-color opacity-100">100%</p>
+          <p className="text-sm md:text-xl font-bold text-primary-color opacity-100">100%</p>
         </div>
       </div>
+
+      <p className="text-xs">PostgresSQL</p>
     </figure>
   );
 }
