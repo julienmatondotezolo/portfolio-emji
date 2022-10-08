e70b8b1ea292028fa7f33f91ad1540c481add1ca
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-10-08 23:01:39 +0200

    adding animation to button

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -29,11 +29,27 @@ export function Header() {
         <SocialIcon url={config.URL.SOCIAL.LINKEDIN} fgColor="gray" bgColor="transparent" />
         <SocialIcon url={`mailto:${config.URL.SOCIAL.EMAIL}`} fgColor="gray" bgColor="transparent" />
       </motion.div>
-      <div className="flex items-center">
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          delay: 0.5,
+          duration: 1.8,
+        }}
+        className="flex items-center"
+      >
         <Link href="#contact">
           <button className="heroButtonInversed mx-5 my-1">Contact</button>
         </Link>
-      </div>
+      </motion.div>
     </header>
   );
 }
