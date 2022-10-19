765c537cf4a1306b0b472fe5d55971baf1b313b8
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-10-20 00:26:36 +0200

    styling carousel

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -22,7 +22,7 @@ export function Projects({ projects }: Props) {
         className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
         data-carousel-prev
       >
-        <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+        <span className="carouselButton">
           <svg
             aria-hidden="true"
             className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
@@ -31,7 +31,7 @@ export function Projects({ projects }: Props) {
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path>
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M15 19l-7-7 7-7"></path>
           </svg>
           <span className="sr-only">Previous</span>
         </span>
@@ -42,7 +42,7 @@ export function Projects({ projects }: Props) {
         className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
         data-carousel-next
       >
-        <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+        <span className="carouselButton">
           <svg
             aria-hidden="true"
             className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
@@ -51,7 +51,7 @@ export function Projects({ projects }: Props) {
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path>
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M9 5l7 7-7 7"></path>
           </svg>
           <span className="sr-only">Next</span>
         </span>
